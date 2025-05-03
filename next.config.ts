9dc1e2d9ec0308402bc2c80b1c032b6c6a09e3ed
Abdul Nelfrank <nelfrankjunior@gmail.com>
2025-05-04 00:55:17 +0300

    Update Next.js configuration for GitHub Pages deployment and add gh-pages dependency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repo = "portfolio"; // Replace with your GitHub repo name
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
 };
 
 export default nextConfig;
