c1ea90540bb0220a73b118a876605cf4e5bb2250
Abdul Nelfrank <nelfrankjunior@gmail.com>
2025-05-04 01:11:28 +0300

    Refactor Next.js configuration and remove unused dependencies from package.json

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const repo = "portfolio"; // Replace with your GitHub repo name
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
+  /* config options here */
 };
 
 export default nextConfig;
